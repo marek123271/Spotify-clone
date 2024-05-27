6e179d1f309355d024a804e3804e2098ffd3f349
Marek Sroczyński <marek123271@gmail.com>
2024-05-27 14:10:36 +0100

    have code on screen

--- client/spotify-clone/src/App.js
+++ client/spotify-clone/src/App.js
@@ -1,10 +1,13 @@
+// App.js
 import React from 'react';
 import Login from './Login';
+import Dashboard from './Dashbord';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const code = new URLSearchParams(window.location.search).get('code');
 
 function App() {
-  return (
-    <Login/>
-  );
+  return code ? <Dashboard code={code} /> : <Login />;
 }
 
 export default App;
---
+++ client/spotify-clone/src/Dashbord.js
@@ -0,0 +1,11 @@
+// Dashboard.js
+import React from 'react';
+
+export default function Dashboard({ code }) {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Code: {code}</p>
+    </div>
+  );
+}
